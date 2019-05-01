98ec0b4a57908f7967ff653ca944ade90a15587a
tcool <padds1st@gmail.com>
2019-05-01 15:35:00 +0900

    trying

--- src/main.lisp
+++ src/main.lisp
@@ -18,6 +18,7 @@
 
 (load-dic *exc-adj* "dict/exc_adj.tsv")
 (load-dic *exc-adv* "dict/exc_adv.tsv")
+
 (load-dic *exc-noun* "dict/exc_noun.tsv")
 (load-dic *exc-verb* "dict/exc_verb.tsv")
 (load-dic *index-adv* "dict/index_adv.tsv")
@@ -26,17 +27,18 @@
 
 (defvar *rules*
 '((noun
-   (("s" . "" )("ses" . "s")("ves" . "f")
-    ("xes" . "x")("zes" . "z")("ches" . "ch")
-    ("shes" . "sh")("men" . "man")("ies" . "y")))
+   (("ses" . "s")("ves" . "f")("xes" . "x")
+    ("zes" . "z")("ches" . "ch")("shes" . "sh")
+    ("men" . "man")("ies" . "y")("s" . "" )))
   (verb
-   (("s" . "" )("ies" . "y")("es" . "e")
+   (("ies" . "y")("es" . "e")
     ("es" . "")("ed" . "e")("ed" . "")
-    ("ing" . "e")("ing" . "" )))
+    ("ing" . "e")("ing" . "" )("s" . "" )))
   (adj
    (("er" . "")("est" . "")("er" . "e")("est" . "e")))
   (adv
    (("er" . "")("est" . "")("er" . "e")("est" . "e")))))
+
 
 (defun lemma()
   (ppcre:scan "ses" "buses"))
@@ -58,3 +60,24 @@
 ;; (ppcre:scan "s" "looks")
 ;; 4
 ;; 5
+
+#|
+辞書の作成
+・@wordlistsと@exceptions、2つのハッシュを辞書として作成。
+・それぞれ、次のような構造。これらにデータを登録する。
+{:noun=>{}, :verb=>{}, :adj=>{}, :adv=>{}, :abbr=>{}, :unknown=>{}}
+・wordlistsに見出し語を登録する目的は、品詞を特定せずにlemmaを呼び出した際、どの品詞に属しているかを特定するため。
+
+lemmaメソッドの呼び出し
+<品詞を特定する場合>
+・@exceptionsに単語があれば、そのデータを元に原形を返す
+・なければ、morphological substitution (形態論の置き換え)のルールにしたがって置き換える。
+
+<品詞を特定しない場合>
+・動詞->名詞->形容詞->副詞の順で[:verb, :noun, :adj, :adv, :abbr]、見出し語から品詞を特定する。
+・@exceptionsに単語があれば、そこから原形を返す
+・動詞で見出し語がeで終わっていて、検索する語がedで終わっている場合、
+　edを取らずにdだけをとる。
+・なければ、morphological substitution (形態論の置き換え)のルールにしたがって置き換える。
+
+#|
