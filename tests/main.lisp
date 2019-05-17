821d2c1f3c5a4051cc221e1f56213d5b9f16526f
tcool <padds1st@gmail.com>
2019-05-17 17:52:42 +0900

    add test

--- tests/main.lisp
+++ tests/main.lisp
@@ -1,11 +1,26 @@
-(defpackage lemmatizer/tests/main
+(defpackage cl-lemma/tests/main
   (:use :cl
-        :lemmatizer
+        :cl-lemma
         :rove))
-(in-package :lemmatizer/tests/main)
+(in-package cl-lemma/tests/main)
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :lemmatizer)' in your Lisp.
 
-(deftest test-target-1
-  (testing "should (= 1 1) to be true"
-    (ok (= 1 1))))
+(deftest lemmatize
+  (testing "noun"
+	   (ok (equal (cl-lemma:lemma "leaves" :noun) "leaf"))
+	   (ok (equal (cl-lemma:lemma "libraries" :noun) "library"))
+	   )
+  (testing "verb"
+	   (ok (equal (cl-lemma:lemma "leaves" :verb) "leave"))
+	   (ok (equal (cl-lemma:lemma "went" :verb) "go"))
+	   )
+  (testing "adjective"
+	   (ok (equal (cl-lemma:lemma "better" :adj) "good"))
+	   )
+  (testing "adverb"
+	   (ok (equal (cl-lemma:lemma "better" :adv) "well"))
+	   )
+)
+  
+	       
